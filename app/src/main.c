c04d191bdd87b149a13f13f2519a663b20b40073
Oleg Pocheptsov <opocheptsov@topcon.com>
2026-04-11 14:55:47 +0200

    tmp

--- app/src/main.c
+++ app/src/main.c
@@ -7,7 +7,7 @@
 #include "display.h"
 
 #define LED0_NODE DT_ALIAS(led0)
-
+#define EPD_NODE DT_NODELABEL(epd_mipi)
 int main(void)
 {
     const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
@@ -17,9 +17,18 @@ int main(void)
         return 0;
     }
 
+    	const struct device *epd = DEVICE_DT_GET(EPD_NODE);
+	int ret;
+
+	// k_msleep(3000);
+	printk("late init start\n");
+__asm__ volatile("bkpt 0");
+	ret = device_init(epd);
+	printk("device_init ret=%d\n", ret);
+
     gpio_pin_configure_dt(&led0, GPIO_OUTPUT_INACTIVE);
 
-    display_init();
+    // display_init();
 
 
     printk("Init ok\n");
